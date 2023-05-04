4813e56537343f4a454fe516aba2a4fdf204bd85
carlosgodspeed <carlosminibics@gmail.com>
2023-05-04 14:48:11 -0300

    atualização

--- index.php
+++ index.php
@@ -40,11 +40,20 @@
                   <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
 
-                 <? if(isset($_GET['login']) && $_GET['login'] == 'erro'); { ?>
+                 <? if(isset($_GET['login']) && $_GET['login'] == 'erro') { ?>
 
                  <div class="text-danger">
                   Usuário ou senha inválido(s)
                  </div>
+
+                 <? } ?>
+
+                 <? if(isset($_GET['login']) && $_GET['login'] == 'erro2') { ?>
+
+                 <div class="text-danger">
+                   Faça login antes de acessar as páginas protegidas
+                 </div>
+
                  <? } ?>
                 
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
--- valida_login.php
+++ valida_login.php
@@ -1,12 +1,14 @@
 <?php
 
+     session_start();
+     
      //variavel que verifica se a autenticação foi realizada
      $usuario_autenticado = false;
 
      //usuarios do sistema
      $usuarios_app = array(
-        array('email' => 'carlosminibics@gmail.com', 'senha' => '150700ch'),
-        array('email' => 'user@teste.com.br', 'senha' => '150700ch'),
+        array('email' => 'carlosminibics@gmail.com', 'senha' => '123456'),
+        array('email' => 'user@teste.com.br', 'senha' => '123456'),
      );
 
      /*
@@ -24,7 +26,9 @@
 
     if($usuario_autenticado) {
         echo 'Usuário autenticado';
+        $_SESSION['autenticado'] = 'SIM';
     }else{
+        $_SESSION['autenticado'] = 'NÃo';
         header('Location: index.php?login=erro');
     }
 
