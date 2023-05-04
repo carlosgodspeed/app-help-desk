4a52ffd0a08ad7a764e209edd28cad3df9ff4e07
carlosgodspeed <carlosminibics@gmail.com>
2023-05-04 19:43:18 -0300

    nova atualização

---
+++ registra_chamado.php
@@ -0,0 +1,16 @@
+<?php
+
+     echo '<pre>';
+     print_r($_POST);
+     echo '</pre>';
+
+
+     //fopen('arquivo.hd', 'a');
+
+     $titulo = str_replace('#', '-', $_POST['titulo']);
+     $categoria = str_replace('#', '-', $_POST['categoria']);
+     $descricao = str_replace('#', '-', $_POST['descricao']);
+
+     $texto = $titulo . '#' . $categoria . '#' . $descricao;
+     echo $texto;
+?>
